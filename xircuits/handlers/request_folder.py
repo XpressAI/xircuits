fe736797f9032e47b76feadb694c40a5562890de
MFA-X-AI <68586800+MFA-X-AI@users.noreply.github.com>
2022-03-29 12:41:33 +0800

    fixed tqdm progress bar in request_folder

--- xircuits/handlers/request_folder.py
+++ xircuits/handlers/request_folder.py
@@ -15,17 +15,19 @@ def request_folder(folder, repo_name="XpressAi/Xircuits"):
         print(folder + " already exists.")
 
     contents = repo.get_contents(folder)
-    total_folders = sum(content.type=='dir' for content in contents)
-    
-    pbar = tqdm(total=total_folders)
+        
+    urls = {}
     
     while len(contents)>0:
         file_content = contents.pop(0)
         if file_content.type=='dir':
-            os.mkdir(file_content.path)
+            if not os.path.exists(file_content.path):
+                os.mkdir(file_content.path)
             contents.extend(repo.get_contents(file_content.path))
-            pbar.update(1)
 
         else:
             file_url = base_url + "/" + file_content.path
-            request.urlretrieve(file_url, file_content.path)
+            urls.update({file_url: file_content.path})
+
+    for url in tqdm(urls):
+        request.urlretrieve(url, urls[url])
