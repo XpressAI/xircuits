ac2de6294d6e271813b40d859d3d944ff7290557
AdrySky <adry.aqil@skymind.my>
2022-03-30 11:03:03 +0800

    Create custom and triangle link

---
+++ src/components/link/CustomLinkFactory.tsx
@@ -0,0 +1,70 @@
+import { DefaultLinkFactory } from '@projectstorm/react-diagrams';
+import * as React from 'react';
+import { CustomLinkModel, TriangleLinkModel } from './CustomLinkModel';
+import styled from '@emotion/styled';
+import { css, keyframes } from '@emotion/react';
+
+namespace S {
+	export const Keyframes = keyframes`
+		from {
+			stroke-dashoffset: 24;
+		}
+		to {
+			stroke-dashoffset: 0;
+		}
+	`;
+
+	const selected = css`
+		stroke-dasharray: 10, 2;
+		animation: ${Keyframes} 1s linear infinite;
+	`;
+
+	export const Path = styled.path<{ selected: boolean }>`
+		${(p) => p.selected && selected};
+		fill: none;
+		pointer-events: auto;
+	`;
+}
+
+export class CustomLinkFactory extends DefaultLinkFactory {
+	constructor() {
+		super('custom');
+	}
+
+	generateModel(): CustomLinkModel {
+		return new CustomLinkModel();
+	}
+
+	generateLinkSegment(model: CustomLinkModel, selected: boolean, path: string) {
+		return (
+			<S.Path
+				selected={selected}
+				stroke={selected ? 'yellow' : model.getOptions().color}
+				strokeWidth={model.getOptions().width}
+				d={path}
+			/>
+		);
+	}
+}
+
+export class TriangleLinkFactory extends DefaultLinkFactory {
+	constructor() {
+		super('triangle');
+	}
+
+	generateModel(): TriangleLinkModel {
+		return new TriangleLinkModel();
+	}
+
+	generateLinkSegment(model: TriangleLinkModel, selected: boolean, path: string) {
+		return (
+			<S.Path
+				selected={!selected}
+				stroke={!selected ? model.getOptions().selectedColor : 'yellow'}
+				strokeWidth={model.getOptions().width}
+				d={path}
+			/>
+		);
+	}
+}
+
