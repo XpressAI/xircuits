7c3b3a8480a536086dfa09af0571a6258cedd2bf
MansourAlawi <mansour.alawi@gmail.com>
2022-04-05 10:41:46 +0800

    add_sample_component

--- xai_components/xai_pycaret/utils.py
+++ xai_components/xai_pycaret/utils.py
@@ -88,6 +88,46 @@ class SampleTestData(Component):
 
         self.done = True
 
+
+"""
+This component sample a set numbers of rows from the dataframe
+"""
+@xai_component(color="green")
+class SampleData(Component):
+    in_dataset: InArg[any] 
+    sample_size: InArg[float] #Fraction of testing dataset size.
+    seed : InArg[int] #You can use random_state for reproducibility.
+
+    sampled_dataset : OutArg[any] #sampled dataset 
+    
+    def __init__(self):
+
+        self.done = False
+        self.in_dataset = InArg(None)
+        self.sample_size = InArg(None)
+        self.seed = InArg(None)
+        
+        self.sampled_dataset = OutArg(None)
+
+    def execute(self, ctx) -> None:
+
+        in_dataset = self.in_dataset.value
+        sample_size = self.sample_size.value
+        seed = self.seed.value
+
+        if seed is None:
+            print("Set the seed value for reproducibility.")
+
+        sampled_dataset = in_dataset.sample(sample_size, random_state=seed).reset_index(drop=True)
+
+        print('Sampled Data : ' + str(sampled_dataset.shape))
+        
+        self.sampled_dataset.value = sampled_dataset
+
+        self.done = True
+
+
+
 '''
 Logging all the trained models to MLflow, can access at localhost:5000
 '''
