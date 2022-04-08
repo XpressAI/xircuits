dbe41b14af0bee250ed03e7cb1845aebc47e0477
MFA-X-AI <68586800+MFA-X-AI@users.noreply.github.com>
2022-04-08 15:58:56 +0800

    added handler for pulling folder from branch

--- xircuits/handlers/request_folder.py
+++ xircuits/handlers/request_folder.py
@@ -2,12 +2,12 @@ from tqdm import tqdm
 import os
 from urllib import request
 
-def request_folder(folder, repo_name="XpressAi/Xircuits"):
+def request_folder(folder, repo_name="XpressAi/Xircuits", branch="master"):
     from github import Github
     print("Downloading " + folder + " from " + repo_name)
     g = Github()
     repo = g.get_repo(repo_name)
-    base_url = "https://raw.githubusercontent.com/" + repo_name + "/master/"
+    base_url = "https://raw.githubusercontent.com/" + repo_name + "/" + branch
 
     if not os.path.exists(folder):
         os.mkdir(folder)
