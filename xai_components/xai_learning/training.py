d41a64003b1ddbfd192d2e6867a57936aac1993b
annabelle1217 <annabelle.ng@skymind.com>
2022-05-11 17:32:51 +0800

    minor fix "tests" to "test"

--- xai_components/xai_learning/training.py
+++ xai_components/xai_learning/training.py
@@ -19,17 +19,15 @@ class ReadDataSet(Component):
     dataset: OutArg[Tuple[np.array, np.array]]
     class_dict: OutArg[any]
 
-
     def __init__(self):
         self.done = False
         self.dataset_name = InArg.empty()
         self.dataset = OutArg.empty()
         self.class_dict = OutArg.empty()
 
-
     def execute(self, ctx) -> None:
 
-        if self.dataset_name.value == 'mnist':
+        if self.dataset_name.value == "mnist":
             (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
             # Scale images to the [0, 1] range
@@ -43,8 +41,11 @@ class ReadDataSet(Component):
             new_y = np.concatenate((y_train, y_test), axis=None)
             self.dataset.value = (new_x, new_y)
 
-        elif self.dataset_name.value == 'fashion_mnist':
-            (x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
+        elif self.dataset_name.value == "fashion_mnist":
+            (x_train, y_train), (
+                x_test,
+                y_test,
+            ) = keras.datasets.fashion_mnist.load_data()
 
             # Scale images to the [0, 1] range
             x_train = x_train.astype("float32") / 255
@@ -57,7 +58,7 @@ class ReadDataSet(Component):
             new_y = np.concatenate((y_train, y_test), axis=None)
             self.dataset.value = (new_x, new_y)
 
-        elif self.dataset_name.value == 'cifar10':
+        elif self.dataset_name.value == "cifar10":
             (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
 
             # Normalize pixel values to be between 0 and 1
@@ -68,7 +69,7 @@ class ReadDataSet(Component):
 
             self.dataset.value = (new_x, new_y)
 
-        elif self.dataset_name.value == 'cifar100':
+        elif self.dataset_name.value == "cifar100":
             (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
 
             # Normalize pixel values to be between 0 and 1
@@ -78,14 +79,17 @@ class ReadDataSet(Component):
             new_y = np.concatenate((y_train, y_test), axis=None)
 
             self.dataset.value = (new_x, new_y)
-
 
         elif self.dataset_name.value:
             try:
                 import cv2
+
                 BASE_FOLDER = self.dataset_name.value
-                folders = [os.path.join(BASE_FOLDER, folder) for folder in os.listdir(BASE_FOLDER)]
-                
+                folders = [
+                    os.path.join(BASE_FOLDER, folder)
+                    for folder in os.listdir(BASE_FOLDER)
+                ]
+
                 print(f"Detecting {len(folders)} classes in {BASE_FOLDER}.")
                 # lists to store data
                 data = []
@@ -101,13 +105,16 @@ class ReadDataSet(Component):
                             data.append(img)
                             label.append(folder)
 
-                        except: 
-                            print(f'Error reading file: {os.path.abspath(file)}. Skipping...')                        
+                        except:
+                            print(
+                                f"Error reading file: {os.path.abspath(file)}. Skipping..."
+                            )
 
                 new_x = np.asarray(data)
 
                 # Import label encoder
                 from sklearn import preprocessing
+
                 label_encoder = preprocessing.LabelEncoder()
                 new_y = label_encoder.fit_transform(label)
 
@@ -115,20 +122,20 @@ class ReadDataSet(Component):
 
                 self.dataset.value = (new_x, new_y)
 
-            except Exception as e: 
+            except Exception as e:
                 print(e)
 
         else:
             print("Dataset was not found!")
 
-
         self.done = True
+
 
 @xai_component(type="in")
 class ReadMaskDataSet(Component):
     dataset_name: InArg[str]
     mask_dataset_name: InArg[str]
-    
+
     dataset: OutArg[Tuple[str, str]]
 
     def __init__(self):
@@ -140,7 +147,7 @@ class ReadMaskDataSet(Component):
     def execute(self, ctx) -> None:
 
         if self.dataset_name.value and self.mask_dataset_name.value:
-            
+
             # Preprocessing can be done here if needed
             self.dataset.value = (self.dataset_name.value, self.mask_dataset_name.value)
             print(self.dataset.value)
@@ -175,12 +182,12 @@ class FlattenImageData(Component):
 
 @xai_component(type="split")
 class TrainTestSplit(Component):
-    dataset: InArg[any] #Tuple[np.array, np.array]
+    dataset: InArg[any]  # Tuple[np.array, np.array]
     train_split: InArg[float]
     random_state: InArg[int]
     shuffle: InArg[bool]
-    train: OutArg[Tuple[np.array, np.array]] #Tuple[np.array, np.array]
-    test: OutArg[Tuple[np.array, np.array]] #Tuple[np.array, np.array]
+    train: OutArg[Tuple[np.array, np.array]]  # Tuple[np.array, np.array]
+    test: OutArg[Tuple[np.array, np.array]]  # Tuple[np.array, np.array]
 
     def __init__(self):
         self.done = False
@@ -196,9 +203,17 @@ class TrainTestSplit(Component):
         train_split = self.train_split.value if self.train_split.value else 0.75
         shuffle = self.shuffle.value if self.shuffle.value else True
         random_state = self.random_state.value if self.random_state.value else None
-        print(f"Split Parameters:\nTrain Split {train_split} \nShuffle: {shuffle} \nRandom State: {random_state}")
-        splits = train_test_split(self.dataset.value[0], self.dataset.value[1], test_size=train_split, shuffle=shuffle, random_state=random_state)
-        
+        print(
+            f"Split Parameters:\nTrain Split {train_split} \nShuffle: {shuffle} \nRandom State: {random_state}"
+        )
+        splits = train_test_split(
+            self.dataset.value[0],
+            self.dataset.value[1],
+            test_size=train_split,
+            shuffle=shuffle,
+            random_state=random_state,
+        )
+
         train_x = splits[0]
         test_x = splits[1]
         train_y = splits[2]
@@ -210,6 +225,7 @@ class TrainTestSplit(Component):
         self.train.value = train
         self.test.value = test
         self.done = True
+
 
 @xai_component(type="model")
 class Create1DInputModel(Component):
@@ -226,21 +242,22 @@ class Create1DInputModel(Component):
         x_shape = self.training_data.value[0].shape
         y_shape = self.training_data.value[1].shape
 
-        model = keras.Sequential([
-            keras.layers.Dense(512, activation='relu', input_shape=(x_shape[1],)),
-           keras.layers.Dropout(rate=0.5),
-            keras.layers.Dense(y_shape[1], activation='softmax')
-        ])
+        model = keras.Sequential(
+            [
+                keras.layers.Dense(512, activation="relu", input_shape=(x_shape[1],)),
+                keras.layers.Dropout(rate=0.5),
+                keras.layers.Dense(y_shape[1], activation="softmax"),
+            ]
+        )
 
         model.compile(
-            loss='categorical_crossentropy',
-            optimizer='adam',
-            metrics=['accuracy']
+            loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
         )
 
         self.model.value = model
 
         self.done = True
+
 
 @xai_component(type="model")
 class Create2DInputModel(Component):
@@ -249,13 +266,11 @@ class Create2DInputModel(Component):
     model: OutArg[keras.Sequential]
     model_config: OutArg[dict]
 
-
     def __init__(self):
         self.done = False
         self.training_data = InArg.empty()
         self.model = OutArg.empty()
         self.model_config = OutArg.empty()
-
 
     def execute(self, ctx) -> None:
 
@@ -278,15 +293,13 @@ class Create2DInputModel(Component):
         )
 
         model.compile(
-            loss='categorical_crossentropy',
-            optimizer='adam',
-            metrics=['accuracy']
+            loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
         )
 
         model_config = {
-            'lr': model.optimizer.lr.numpy().item(),
-            'optimizer_name': model.optimizer._name,
-            'loss': model.loss,
+            "lr": model.optimizer.lr.numpy().item(),
+            "optimizer_name": model.optimizer._name,
+            "loss": model.loss,
         }
 
         self.model.value = model
@@ -297,8 +310,8 @@ class Create2DInputModel(Component):
 
 @xai_component(type="train")
 class TrainImageClassifier(Component):
-    model: InArg[any] #keras.Sequential
-    training_data: InArg[any] #Tuple[np.array, np.array]
+    model: InArg[any]  # keras.Sequential
+    training_data: InArg[any]  # Tuple[np.array, np.array]
     training_epochs: InArg[int]
 
     trained_model: OutArg[keras.Sequential]
@@ -321,14 +334,17 @@ class TrainImageClassifier(Component):
             self.training_data.value[0],
             self.training_data.value[1],
             batch_size=32,
-            epochs=self.training_epochs.value
+            epochs=self.training_epochs.value,
         )
 
         # Set training metrics
         training_metrics = {}
         for key in train.history.keys():
             training_metrics[key] = {}
-            [training_metrics[key].update({i + 1: v}) for i, v in enumerate(train.history[key])]
+            [
+                training_metrics[key].update({i + 1: v})
+                for i, v in enumerate(train.history[key])
+            ]
 
         self.trained_model.value = model
         self.training_metrics.value = training_metrics
@@ -337,8 +353,8 @@ class TrainImageClassifier(Component):
 
 @xai_component(type="eval")
 class EvaluateAccuracy(Component):
-    model: InArg[any] #keras.Sequential
-    eval_dataset: InArg[any] #Tuple[np.array, np.array]
+    model: InArg[any]  # keras.Sequential
+    eval_dataset: InArg[any]  # Tuple[np.array, np.array]
 
     metrics: OutArg[Dict[str, str]]
 
@@ -349,11 +365,10 @@ class EvaluateAccuracy(Component):
         self.metrics = OutArg.empty()
 
     def execute(self, ctx) -> None:
-        (loss, acc) = self.model.value.evaluate(self.eval_dataset.value[0], self.eval_dataset.value[1], verbose=0)
-        metrics = {
-            'loss': str(loss),
-            'accuracy': str(acc)
-        }
+        (loss, acc) = self.model.value.evaluate(
+            self.eval_dataset.value[0], self.eval_dataset.value[1], verbose=0
+        )
+        metrics = {"loss": str(loss), "accuracy": str(acc)}
         print(metrics)
 
         self.metrics.value = metrics
@@ -381,19 +396,20 @@ class ShouldStop(Component):
         self.retries += 1
 
         if self.retries < self.max_retries.value:
-            the_accuracy = float(self.metrics.value['accuracy'])
-            print('Eval accuracy:' + str(the_accuracy))
+            the_accuracy = float(self.metrics.value["accuracy"])
+            print("Eval accuracy:" + str(the_accuracy))
 
             if the_accuracy < self.target_accuracy.value:
-                print('Will retrain')
+                print("Will retrain")
                 self.should_retrain.value = True
             else:
-                print('Target accuracy achieved')
+                print("Target accuracy achieved")
                 self.should_retrain.value = False
         else:
-            print('Unable to achieve target accuracy.  Giving up.')
+            print("Unable to achieve target accuracy.  Giving up.")
             self.should_retrain.value = False
         self.done = True
+
 
 @xai_component
 class SaveKerasModel(Component):
@@ -411,7 +427,11 @@ class SaveKerasModel(Component):
 
     def execute(self, ctx) -> None:
         model = self.model.value
-        model_name = self.model_name.value if self.model_name.value else os.path.splitext(sys.argv[0])[0] + ".h5"
+        model_name = (
+            self.model_name.value
+            if self.model_name.value
+            else os.path.splitext(sys.argv[0])[0] + ".h5"
+        )
         model.save(model_name)
         print(f"Saving Keras h5 model at: {model_name}")
         self.model_h5_path.value = model_name
@@ -434,23 +454,23 @@ class SaveKerasModelInModelStash(Component):
     def execute(self, ctx) -> None:
         config = self.execution_context.args
 
-        if not os.path.exists(os.path.join('..', 'experiments')):
-            os.mkdir(os.path.join('..', 'experiments'))
+        if not os.path.exists(os.path.join("..", "experiments")):
+            os.mkdir(os.path.join("..", "experiments"))
 
-        exp_dir = os.path.join('..', 'experiments', config.name)
+        exp_dir = os.path.join("..", "experiments", config.name)
 
         if os.path.exists(exp_dir):
-            exp_dir = exp_dir + '-' + datetime.now().strftime('%Y%m%d-%H:%M:%S')
+            exp_dir = exp_dir + "-" + datetime.now().strftime("%Y%m%d-%H:%M:%S")
         os.mkdir(exp_dir)
 
-        self.model.value.save(os.path.join(exp_dir, 'model.h5'))
+        self.model.value.save(os.path.join(exp_dir, "model.h5"))
 
         eval_json = json.dumps(self.metrics.value, sort_keys=True, indent=4)
-        with open(os.path.join(exp_dir, 'eval.json'), 'w') as f:
+        with open(os.path.join(exp_dir, "eval.json"), "w") as f:
             f.write(eval_json)
 
         config_json = json.dumps(vars(config), sort_keys=True, indent=4)
-        with open(os.path.join(exp_dir, 'conf.json'), 'w') as f:
+        with open(os.path.join(exp_dir, "conf.json"), "w") as f:
             f.write(config_json)
 
         os.system("git add . && git commit -m 'experiment %s'" % (exp_dir))
