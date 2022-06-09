ffd6a842d4ebf98c822c74e1b0f5e22bf5720b34
AdrySky <adry.aqil@skymind.my>
2022-06-09 10:24:38 +0800

    Initial subprocess operation

--- xircuits/handlers/spark_submit.py
+++ xircuits/handlers/spark_submit.py
@@ -3,7 +3,7 @@ import tornado
 import subprocess
 from jupyter_server.base.handlers import APIHandler
 import platform
-
+import os
 class SparkSubmitRouteHandler(APIHandler):
     @tornado.web.authenticated
     def get(self):
@@ -12,7 +12,8 @@ class SparkSubmitRouteHandler(APIHandler):
     @tornado.web.authenticated
     def post(self):
         input_data = self.get_json_body()
-
+        export_str = ""
+        command_str = ""
         addArguments = input_data["addArgs"]
         currentPath = input_data["currentPath"]
 
@@ -21,8 +22,20 @@ class SparkSubmitRouteHandler(APIHandler):
         else: 
             spark_submit_cmd = "spark-submit "
 
-        spark_submit_str= spark_submit_cmd + addArguments + " " + currentPath
-        process=subprocess.Popen(spark_submit_str,stdout=subprocess.PIPE,stderr=subprocess.PIPE, universal_newlines=True, shell=True)
+        for key, value in addArguments.items():
+            if key == 'export':
+                export_str = value
+            elif key == 'command':
+                command_str = value
+        spark_submit_str= command_str + " " + currentPath
+        # env = {
+        #     **os.environ
+        # }
+        # if export_str != 'null':
+        #     process=subprocess.Popen(spark_submit_str,stdout=subprocess.PIPE,stderr=subprocess.PIPE, env=export_str,universal_newlines=True, shell=True)
+        #     stdout = process.communicate()
+        # else:
+        process=subprocess.Popen(spark_submit_str,stdout=subprocess.PIPE,stderr=subprocess.PIPE,env=dict(os.environ, PATH="path"),universal_newlines=True, shell=True)
         stdout,stderr = process.communicate()
 
         data = {
