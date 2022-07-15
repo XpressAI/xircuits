824382a21f38210cde9ed700c0d4ae5f7de060cd
Muhammad Fahreza Alghifari <fahreza.alghifari@xpress.ai>
2022-07-15 22:46:29 +0700

    added desc for util components

--- xai_components/xai_utils/utils.py
+++ xai_components/xai_utils/utils.py
@@ -3,9 +3,43 @@ from xai_components.base import InArg, OutArg, InCompArg, Component, xai_compone
 import os
 import sys
 from pathlib import Path
+import time
 
 @xai_component
 class ZipDirectory(Component):
+    """Zips a directory.
+    
+    ### inPorts:
+    - zip_fn: the Zip filename.
+        Default: .xircuits canvas name.
+    - dir_name: the directory to be zipped.
+    - include_dir: bundle the specified directory in the zip if True.
+    
+        #### EG:
+
+        Creating Bar.zip which zips the dir `Foo` with the structure:
+        ```  
+        Foo
+        |-- file1
+        |-- file2
+        ```
+
+        if `include_dir` is `True`:
+        ```
+        Bar.zip
+        |-- Foo
+            |-- file1
+            |-- file2
+        ```
+
+        if `include_dir` is `False`:
+        ```
+        Bar.zip
+        |-- file1
+        |-- file2
+        ```
+
+    """
     zip_fn: InArg[str]
     dir_name: InCompArg[str]
     include_dir: InArg[bool]
@@ -23,7 +57,7 @@ class ZipDirectory(Component):
         from tqdm import tqdm
 
         zip_fn = self.zip_fn.value if self.zip_fn.value else Path(sys.argv[0]).stem
-        dir_name = self.dir_name.value if self.dir_name.value else None
+        dir_name = self.dir_name.value
 
         assert os.path.isdir(dir_name), "invalid dir_name!"
 
@@ -59,6 +93,11 @@ class ZipDirectory(Component):
 
 @xai_component
 class DeleteFile(Component):
+    """Deletes a file.
+    
+    ### inPorts:
+    - filename: path to file to be deleted.
+    """
     filename: InCompArg[str]
 
     def __init__(self):
@@ -79,11 +118,21 @@ class DeleteFile(Component):
 
 @xai_component(color="green")
 class TimerComponent(Component):
+    """Chain multiple instances of this component to measure elapsed time. 
     
-    in_timer: InArg[any]
+    ### inPorts:
+    - in_timer: if provided will measure the elapsed time since last called.
+        if not provided, will start a new timer.
+    - timer_message: a log message to be printed. 
+
+    ### outPorts:
+    - out_timer: passes current timer to the next TimerComponent. 
+    - elapsed_time: the elapsed time in seconds.
+    """
+    in_timer: InArg[time.time]
     timer_message: InArg[str]
     
-    out_timer: OutArg[any]
+    out_timer: OutArg[time.time]
     elapsed_time: OutArg[int]
     
     def __init__(self):
@@ -98,9 +147,7 @@ class TimerComponent(Component):
         
 
     def execute(self, ctx) -> None:
-        
-        import time
-        
+                
         current_time = time.time()
         timer_message = self.timer_message.value if self.timer_message.value else "current timer component"
         
@@ -108,7 +155,7 @@ class TimerComponent(Component):
         
         if self.in_timer.value:
                 elapsed_time = current_time - self.in_timer.value
-                print("The elapsed time for " + timer_message + " is " + str(elapsed_time) + "seconds.")
+                print("The elapsed time for " + timer_message + " is " + str(elapsed_time) + " seconds.")
         else:
             print("Starting new timer.")
         
@@ -118,7 +165,12 @@ class TimerComponent(Component):
         
 @xai_component(color="green")
 class SleepComponent(Component):
+    """Pauses the python process.
     
+    ### inPorts:
+    - sleep_timer: the number of seconds to pause.
+        Default `5.0` seconds.
+    """    
     sleep_timer: InArg[float]
 
     def __init__(self):
