e26e7bd78f3abc4a84df811a9cb8e643dc4244bb
Paul Dubs <paul.dubs@gmail.com>
2023-02-20 10:50:34 +0100

    Use absolute module path instead of relative

--- xircuits/compiler/generator.py
+++ xircuits/compiler/generator.py
@@ -8,7 +8,7 @@ if sys.version_info >= (3, 9):
     from ast import unparse
 else:
     import io
-    from .vendor.unparse import Unparser
+    from xircuits.compiler.vendor.unparse import Unparser
 
 
     def unparse(parsed):
