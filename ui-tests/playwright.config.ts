ce36fc8ac32252dda39a9d319e5e4039c4f515c4
MFA-X-AI <fahreza@xpress.ai>
2023-08-07 13:35:18 +0800

    try remove portview

--- ui-tests/playwright.config.ts
+++ ui-tests/playwright.config.ts
@@ -28,9 +28,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-    viewport: { width: 1280, height: 720 },
-
+    trace: 'on-first-retry'
   },
 
   /* Configure projects for major browsers */
