c5b805266af275b698723ff58c98c7e596522b51
MFA-X-AI <fahreza@xpress.ai>
2024-07-06 02:08:48 +0900

    refactor method specific functions to their respective  functional component

--- src/components/node/CustomNodeWidget.tsx
+++ src/components/node/CustomNodeWidget.tsx
@@ -3,14 +3,14 @@ import * as _ from 'lodash';
 import { DiagramEngine } from '@projectstorm/react-diagrams-core';
 import { DefaultNodeModel } from '@projectstorm/react-diagrams';
 import styled from '@emotion/styled';
-import Toggle from 'react-toggle'
+import Toggle from 'react-toggle';
 import { ILabShell, JupyterFrontEnd } from '@jupyterlab/application';
 import { CustomPortLabel } from '../port/CustomPortLabel';
 import { Dialog } from '@jupyterlab/apputils';
 import { formDialogWidget } from '../../dialog/formDialogwidget';
 import { showFormDialog } from '../../dialog/FormDialog';
 import { CommentDialog } from '../../dialog/CommentDialog';
-import ReactTooltip from "react-tooltip"
+import ReactTooltip from "react-tooltip";
 import { marked } from 'marked';
 import Color from 'colorjs.io';
 import { commandIDs } from "../../commands/CommandIDs";
@@ -18,41 +18,41 @@ import { commandIDs } from "../../commands/CommandIDs";
 var S;
 (function (S) {
     S.Node = styled.div<{ borderColor: string, background: string; selected: boolean; }>`
-		background-color: ${(p) => {
-        const color = new Color(p.background);
-        color.alpha = 0.75;
-        color.oklch.c *= 1.2;
-        return color.to('oklch').toString();
-    }};
-    box-shadow: 1px 1px 10px ${(p) => p.selected ? '3px rgb(0 192 255 / 0.5)' : '0px rgb(0 0 0 / 0.5)'};
-		border-radius: 5px;
-		font-family: sans-serif;
-		color: white;
-		overflow: visible;
-		font-size: 11px;
-		border: solid 1px ${(p) => (p.selected ? (p.borderColor == undefined ? 'rgb(0,192,255)' : p.borderColor) : 'black')};
-	`;
+        background-color: ${(p) => {
+            const color = new Color(p.background);
+            color.alpha = 0.75;
+            color.oklch.c *= 1.2;
+            return color.to('oklch').toString();
+        }};
+        box-shadow: 1px 1px 10px ${(p) => p.selected ? '3px rgb(0 192 255 / 0.5)' : '0px rgb(0 0 0 / 0.5)'};
+        border-radius: 5px;
+        font-family: sans-serif;
+        color: white;
+        overflow: visible;
+        font-size: 11px;
+        border: solid 1px ${(p) => (p.selected ? (p.borderColor == undefined ? 'rgb(0,192,255)' : p.borderColor) : 'black')};
+    `;
 
     S.Title = styled.div`
-		background: rgba(0, 0, 0, 0.3);
-		display: flex;
-		white-space: nowrap;
-		justify-items: center;
-    box-shadow: inset 0 -2px 4px 0 rgb(0 0 0 / 0.05);  
-	`;
+        background: rgba(0, 0, 0, 0.3);
+        display: flex;
+        white-space: nowrap;
+        justify-items: center;
+        box-shadow: inset 0 -2px 4px 0 rgb(0 0 0 / 0.05);
+    `;
 
     S.TitleName = styled.div`
-		flex-grow: 1;
-		padding: 5px 5px;
-	`;
+        flex-grow: 1;
+        padding: 5px 5px;
+    `;
 
     S.CommentContainer = styled.div<{ selected: boolean; }>`
         background: rgba(0, 0, 0, 0.3);
         border-radius: 5px;
-		font-family: sans-serif;
-		color: rgb(255, 255, 255);
-		border: solid 2px black;
-		font-size: 12px;
+        font-family: sans-serif;
+        color: rgb(255, 255, 255);
+        border: solid 2px black;
+        font-size: 12px;
         border: solid 2px ${(p) => p.selected ? 'rgb(0,192,255)' : 'black'};
         padding: 5px;
     `;
@@ -63,34 +63,34 @@ var S;
         font-family: 'Roboto', sans-serif;
         font-weight: 700;
         font-size: 13px;
-	`;
+    `;
 
     S.Ports = styled.div`
-		display: flex;
-		background-image: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.2));
-	`;
+        display: flex;
+        background-image: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.2));
+    `;
 
     S.PortsContainer = styled.div`
         max-width: 640px;
         white-space: pre;
-		flex-grow: 1;
-		display: flex;
-		flex-direction: column;
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
 
-		&:first-of-type {
-			margin-right: 10px;
-		}
+        &:first-of-type {
+            margin-right: 10px;
+        }
 
-		&:only-child {
-			margin-right: 0px;
-		}
-	`;
+        &:only-child {
+            margin-right: 0px;
+        }
+    `;
     S.WorkflowNode = styled(S.Node)`
-    outline: 2px solid rgba(255, 255, 255, 0.5);
-    outline-offset: 8px; // Space between the main node and the outline
+        outline: 2px solid rgba(255, 255, 255, 0.5);
+        outline-offset: 8px; // Space between the main node and the outline
 
-    ${(p) => p.selected && `
-        outline: 2px solid rgba(0, 192, 255, 0.5); // blue
+        ${(p) => p.selected && `
+            outline: 2px solid rgba(0, 192, 255, 0.5); // blue
         `}
     `;
 })(S || (S = {}));
@@ -102,33 +102,61 @@ export interface DefaultNodeProps {
     shell: ILabShell;
 }
 
-const CommentNode = ({ node, handleEditComment }) => (
-    <S.CommentContainer onDoubleClick={handleEditComment} selected={node.isSelected()}>
-        <S.TitleName><b>{node.getOptions().name}</b></S.TitleName>
-        <div className='comment-component-content'>
-            {node['extras']['commentInput']}
-        </div>
-    </S.CommentContainer>
-);
+const CommentNode = ({ node }) => {
+    const [commentInput, setCommentInput] = React.useState(node['extras']['commentInput']);
 
-const ParameterNode = ({ node, engine, handleEditLiteral }) => (
-    <S.Node
-        borderColor={node.getOptions().extras["borderColor"]}
-        data-default-node-name={node.getOptions().name}
-        selected={node.isSelected()}
-        background={node.getOptions().color}
-        onDoubleClick={handleEditLiteral}
-    >
-        <S.Title>
-            <S.TitleName>{node.getOptions().name}</S.TitleName>
-        </S.Title>
-        <S.Ports>
-            <S.PortsContainer>{_.map(node.getInPorts(), port => <CustomPortLabel engine={engine} port={port} key={port.getID()} node={node} />)}</S.PortsContainer>
-            <S.PortsContainer>{_.map(node.getOutPorts(), port => <CustomPortLabel engine={engine} port={port} key={port.getID()} node={node} />)}</S.PortsContainer>
-        </S.Ports>
-    </S.Node>
+    const handleEditComment = async () => {
+        let dialogResult = await showFormDialog({
+            body: formDialogWidget(<CommentDialog commentInput={commentInput} />),
+            buttons: [Dialog.cancelButton(), Dialog.okButton({ label: 'Submit' })],
+            focusNodeSelector: 'textarea'
+        });
 
-);
+        if (dialogResult["button"]["label"] === 'Cancel') {
+            return false;
+        }
+
+        const newVal = dialogResult["value"][''];
+        setCommentInput(newVal);
+        node['extras']['commentInput'] = newVal;
+    };
+
+    return (
+        <S.CommentContainer onDoubleClick={handleEditComment} selected={node.isSelected()}>
+            <S.TitleName><b>{node.getOptions().name}</b></S.TitleName>
+            <div className='comment-component-content'>
+                {commentInput}
+            </div>
+        </S.CommentContainer>
+    );
+};
+
+const ParameterNode = ({ node, engine, app }) => {
+    const handleEditLiteral = () => {
+        if (!node.getOptions()["name"].startsWith("Literal")) {
+            return;
+        }
+        app.commands.execute(commandIDs.editNode);
+    };
+
+    return (
+        <S.Node
+            borderColor={node.getOptions().extras["borderColor"]}
+            data-default-node-name={node.getOptions().name}
+            selected={node.isSelected()}
+            background={node.getOptions().color}
+            onDoubleClick={handleEditLiteral}
+        >
+            <S.Title>
+                <S.TitleName>{node.getOptions().name}</S.TitleName>
+            </S.Title>
+            <S.Ports>
+                <S.PortsContainer>{_.map(node.getInPorts(), port => <CustomPortLabel engine={engine} port={port} key={port.getID()} node={node} />)}</S.PortsContainer>
+                <S.PortsContainer>{_.map(node.getOutPorts(), port => <CustomPortLabel engine={engine} port={port} key={port.getID()} node={node} />)}</S.PortsContainer>
+            </S.Ports>
+        </S.Node>
+    );
+};
 
 const StartFinishNode = ({ node, engine, handleDeletableNode }) => (
     <S.Node
@@ -150,7 +178,7 @@ const StartFinishNode = ({ node, engine, handleDeletableNode }) => (
     </S.Node>
 );
 
-const WorkflowNode = ({ node, engine, handleEditLiteral, handleDeletableNode }) => {
+const WorkflowNode = ({ node, engine, handleDeletableNode }) => {
     const elementRef = React.useRef<HTMLElement>(null);
 
     return (
@@ -162,7 +190,6 @@ const WorkflowNode = ({ node, engine, handleEditLiteral, handleDeletableNode }) 
                 data-default-node-name={node.getOptions().name}
                 selected={node.isSelected()}
                 background={node.getOptions().color}
-                onDoubleClick={handleEditLiteral}
             >
                 <S.Title>
                     <S.TitleName>{node.getOptions().name}</S.TitleName>
@@ -179,8 +206,28 @@ const WorkflowNode = ({ node, engine, handleEditLiteral, handleDeletableNode }) 
     );
 };
 
-const ComponentLibraryNode = ({ node, engine, shell, handleEditLiteral, handleDescription, showDescription, descriptionStr, handleDeletableNode, hideErrorTooltip }) => {
+const ComponentLibraryNode = ({ node, engine, shell, handleDeletableNode }) => {
+    const [showDescription, setShowDescription] = React.useState(false);
+    const [descriptionStr, setDescriptionStr] = React.useState("");
     const elementRef = React.useRef<HTMLElement>(null);
+
+    const handleDescription = async () => {
+        setShowDescription(!showDescription);
+        getDescriptionStr();
+        if (elementRef.current) {
+            ReactTooltip.show(elementRef.current);
+        }
+    };
+
+    const getDescriptionStr = () => {
+        let dscrptStr = node['extras']['description'] ?? '***No description provided***';
+        setDescriptionStr(dscrptStr);
+    };
+
+    const hideErrorTooltip = () => {
+        delete node.getOptions().extras["tip"];
+        node.getOptions().extras["borderColor"] = "rgb(0,192,255)";
+    };
 
     return (
         <div style={{ position: "relative" }}>
@@ -205,13 +252,12 @@ const ComponentLibraryNode = ({ node, engine, shell, handleEditLiteral, handleDe
                 data-default-node-name={node.getOptions().name}
                 selected={node.isSelected()}
                 background={node.getOptions().color}
-                onDoubleClick={handleEditLiteral}
             >
                 <S.Title>
                     <S.TitleName>{node.getOptions().name}</S.TitleName>
                     <label data-no-drag>
                         <Toggle className='lock' checked={node.isLocked() ?? false} onChange={event => handleDeletableNode('nodeDeletable', event)} />
-                        <Toggle className='description' name='Description' checked={showDescription ?? false} onChange={event => handleDescription('description', event)} />
+                        <Toggle className='description' name='Description' checked={showDescription ?? false} onChange={handleDescription} />
                     </label>
                 </S.Title>
                 <S.Ports>
@@ -266,19 +312,6 @@ const ComponentLibraryNode = ({ node, engine, shell, handleEditLiteral, handleDe
 };
 
 export class CustomNodeWidget extends React.Component<DefaultNodeProps> {
-    element: React.RefObject<HTMLElement>;
-    state = {
-        nodeDeletable: false,
-        commentInput: this.props.node['extras']['commentInput'],
-        showDescription: false,
-        descriptionStr: "",
-    };
-
-    constructor(props: DefaultNodeProps) {
-        super(props);
-        this.element = React.createRef();
-    }
-
     handleDeletableNode = (key, event) => {
         this.setState({
             [key]: event.target.checked ? this.props.node.setLocked(true) : this.props.node.setLocked(false),
@@ -289,87 +322,38 @@ export class CustomNodeWidget extends React.Component<DefaultNodeProps> {
         this.props.engine.fireEvent({}, 'onChange');
     };
 
-    handleEditLiteral = () => {
-        if (!this.props.node.getOptions()["name"].startsWith("Literal")) {
-            return;
-        }
-        this.props.app.commands.execute(commandIDs.editNode);
-    };
-
-    handleEditComment = async () => {
-        let dialogResult = await showFormDialog({
-            body: formDialogWidget(<CommentDialog commentInput={this.state.commentInput} />),
-            buttons: [Dialog.cancelButton(), Dialog.okButton({ label: 'Submit' })],
-            focusNodeSelector: 'textarea'
-        });
-
-        if (dialogResult["button"]["label"] === 'Cancel') {
-            return false;
-        }
-
-        const newVal = dialogResult["value"][''];
-        this.setState({ commentInput: newVal });
-        this.props.node['extras']['commentInput'] = newVal;
-        this.handleOnChangeCanvas();
-    };
-
-    handleDescription = async () => {
-        await this.setState({ showDescription: !this.state.showDescription });
-        this.getDescriptionStr();
-        if (this.element.current) {
-            ReactTooltip.show(this.element.current);
-        }
-    };
-
-    getDescriptionStr = () => {
-        let dscrptStr = this.props.node['extras']['description'] ?? '***No description provided***';
-        this.setState({ descriptionStr: dscrptStr });
-    };
-
-    hideErrorTooltip = () => {
-        delete this.props.node.getOptions().extras["tip"];
-        this.props.node.getOptions().extras["borderColor"] = "rgb(0,192,255)";
-    };
-
     render() {
-        const { node, engine, shell } = this.props;
-        const { showDescription, descriptionStr } = this.state;
+        const { node, engine, app, shell } = this.props;
 
         if (node['extras']['type'] === 'comment') {
-            return <CommentNode node={node} handleEditComment={this.handleEditComment} />;
+            return <CommentNode node={node} />;
         }
 
         if (node.getOptions()["name"].startsWith('Literal') || node.getOptions()["name"].startsWith('Argument')) {
-            return <ParameterNode node={node} engine={engine} handleEditLiteral={this.handleEditLiteral} />;
+            return <ParameterNode node={node} engine={engine} app={app} />;
         }
 
         if (node['extras']['type'] === 'xircuits_workflow') {
-            return <WorkflowNode 
-                node={node} 
-                engine={engine} 
-                handleEditLiteral={this.handleEditLiteral} 
-                handleDeletableNode={this.handleDeletableNode} 
+            return <WorkflowNode
+                node={node}
+                engine={engine}
+                handleDeletableNode={this.handleDeletableNode}
             />;
         }
 
         if (node.getOptions()["name"] === 'Start' || node.getOptions()["name"] === 'Finish') {
-            return <StartFinishNode 
-                node={node} 
-                engine={engine} 
-                handleDeletableNode={this.handleDeletableNode} 
+            return <StartFinishNode
+                node={node}
+                engine={engine}
+                handleDeletableNode={this.handleDeletableNode}
             />;
         }
-        
-        return <ComponentLibraryNode 
-            node={node} 
-            engine={engine} 
-            shell={shell} 
-            handleEditLiteral={this.handleEditLiteral} 
-            handleDescription={this.handleDescription} 
-            showDescription={showDescription} 
-            descriptionStr={descriptionStr} 
-            handleDeletableNode={this.handleDeletableNode} 
-            hideErrorTooltip={this.hideErrorTooltip} 
+
+        return <ComponentLibraryNode
+            node={node}
+            engine={engine}
+            shell={shell}
+            handleDeletableNode={this.handleDeletableNode}
         />;
     }
 }
