18cda523a56c540754c9afa7199d103139c35940
MFA-X-AI <fahreza@xpress.ai>
2024-10-04 19:30:42 +0900

    trigger filebrowser refresh on context menu compile

--- src/index.tsx
+++ src/index.tsx
@@ -371,6 +371,7 @@ const xircuits: JupyterFrontEndPlugin<void> = {
             await compileXircuitsFile(xircuitsFile.path);
           }
         }
+        browserFactory.tracker.currentWidget.model.refresh();
       }
     });
 
