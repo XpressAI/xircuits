8ce5e00297a475253f13750adb11983cc16d6f3d
rabea-Al <rabea.alawi@gmail.com>
2025-07-09 17:51:24 -0400

    Restore console log message

--- src/tray_library/Component.tsx
+++ src/tray_library/Component.tsx
@@ -14,6 +14,7 @@ export function initComponentFetcher(app: JupyterFrontEnd) {
 }
 
 export async function fetchComponents() {
+  console.log("Fetching all components... this might take a while.")
   try {
     const componentsResponse = await requestAPI<any>('components/');
     const components = componentsResponse["components"];
