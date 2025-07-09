b107b4a431994aa9d77d53e0e292db6d1730e296
rabea-Al <rabea.alawi@gmail.com>
2025-07-09 17:30:18 -0400

    Show error toast on component parse failure with clickable file/line navigation

--- src/tray_library/Component.tsx
+++ src/tray_library/Component.tsx
@@ -1,18 +1,30 @@
 import { Notification } from '@jupyterlab/apputils';
 import { requestAPI } from "../server/handler";
+import { linkErrorToast } from '../helpers/errorToastLinks';   
+import { JupyterFrontEnd }   from '@jupyterlab/application';   
+
 let componentsCache = {
   data: null
 };
 
+let _app: JupyterFrontEnd | null = null;                       
+
+export function initComponentFetcher(app: JupyterFrontEnd) {   
+  _app = app;
+}
+
 export async function fetchComponents() {
-  console.log("Fetching all components... this might take a while.")
   try {
     const componentsResponse = await requestAPI<any>('components/');
     const components = componentsResponse["components"];
-    const error_msg = componentsResponse["error_msg"];
+    const error_info = componentsResponse["error_info"];
+    if (error_info) {
+      const uniqueId = `${error_info.full_path}:${error_info.line}`;
+      const formatted =
+        `Error found in: ${uniqueId}\n❌ ${error_info.message}`;
 
-    if (error_msg) {
-      Notification.error(error_msg, { autoClose: 3000 });
+      Notification.error(formatted, { autoClose: 6000 });
+      linkErrorToast(uniqueId, error_info.full_path, error_info.line, _app);
     }
     console.log("Fetch complete.")
     return components;
