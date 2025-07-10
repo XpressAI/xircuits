d30be77ab8f2bbf5dbf94a3ea9ac7bf40eef4ad2
rabea-Al <rabea.alawi@gmail.com>
2025-07-10 10:16:15 -0400

    Organize error toast to show file, line, and error type

--- src/tray_library/Component.tsx
+++ src/tray_library/Component.tsx
@@ -1,5 +1,6 @@
 import { Notification } from '@jupyterlab/apputils';
 import { requestAPI } from "../server/handler";
+
 let componentsCache = {
   data: null
 };
@@ -9,10 +10,12 @@ export async function fetchComponents() {
   try {
     const componentsResponse = await requestAPI<any>('components/');
     const components = componentsResponse["components"];
-    const error_msg = componentsResponse["error_msg"];
+    const error_info = componentsResponse["error_info"];
+    if (error_info) {
+      const uniqueId = `${error_info.full_path}\nLine:${error_info.line}`;
+      const formatted =`Error found in: ${uniqueId}\n${error_info.message}`;
 
-    if (error_msg) {
-      Notification.error(error_msg, { autoClose: 3000 });
+      Notification.error(formatted, { autoClose: 6000 });
     }
     console.log("Fetch complete.")
     return components;
